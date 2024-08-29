8c89d40d5e6b8730917302bd1996970b074a0f1b
Kishor Nishad <kishorr127.0@gmail.com>
2024-08-29 13:58:12 +0530

    refactor navbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ export default function Navbar() {
         <div className="bg-black text-white sticky top-0 z-[100]">
             <nav className="max-w-[1450px] mx-auto py-2 flex justify-between mx-2 items-center">
                 <Link to={"/"} className="flex items-center gap-0"> 
-                    <div className="px-3 border-r-2 flex items-center gap-2">
+                    <div className="px-3 lg:border-r-2 flex items-center gap-2">
                         <div className="rounded-md overflow-hidden">
-                        <img src={Logo} className="h-[25px] w-[25px] hidden lg:block" alt="" />
+                        <img src={Logo} className="h-[25px] w-[25px]" alt="" />
                         </div>
-                        Abstract</div>
-                    <div className="px-3">Help Center</div>
+                        <span className="hidden lg:block">Abstract</span></div>
+                    <div className="px-3 hidden lg:block">Help Center</div>
                 </Link>
                 <Link to="/create-card">
-                    <button className="border border-slate-400 rounded-md text-sm py-1 px-4">
+                    <button className="border border-slate-400 rounded-md text-sm py-1 px-4 min-w-[140px]">
                         Submit a request
                     </button>
                 </Link>
