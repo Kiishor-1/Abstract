6802e87a37e86fd0a789f91a61e67385aba3b94b
Kishor Nishad <kishorr127.0@gmail.com>
2024-08-29 14:09:40 +0530

    search input UI

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection({ onSearch }) {
   return (
     <section className="bg-[#E6E6FA] h-[250px] flex flex-col items-center justify-center gap-6">
       <p className="lg:text-6xl text-4xl">How can we help?</p>
-      <div className="flex items-center justify-between border border-slate-700 p-2 w-[40%]">
+      <div className="flex items-center justify-between border border-slate-700 p-2 lg:w-[40%] w-[90%]">
         <input
           type="text"
           className="bg-transparent w-full outline-none h-full px-3"
